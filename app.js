7382f187bd1c26717dcc9657ff097ce5effdea4c
1bubneb <1bubneb@bitbucket.org>
2017-12-02 00:29:32 -0500

    Added permissions to giving cookies, and accidentally to displaying

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const randomInt = require('random-int');
 const cookies = require('./config/cookies.json');
 const auth = require('./config/auth.json');
+const givers = require('./config/givers.json');
 
 var users = JSON.parse( fs.readFileSync("./config/userCookies.json", "utf8") );
 
@@ -11,16 +12,27 @@ var bot = new Discord.Client({ token: auth.token, autorun: true });
 bot.on('ready', function (evt) {
   console.log('Cookiebot online!');
   bot.setPresence({game: {name: 'baking cookies'}});
+  
+  
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
+  var canGive = false;
+  
+  for(i in givers)
+  {
+    if(userID === givers[i])
+      canGive = true;
+  }
+  
+  
   if (message.substring(0, 1) == '!')
   {
   	var args = message.substring(1).split(' ');
   	var cmd = args[0].toLowerCase();
     var temp = args[1];
     var id = '';
-
+    
     for(i in temp)
     {
       if((i > 2) && (i < (temp.length - 1)))
@@ -33,7 +45,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       case 'givecookie':
         bot.sendMessage({
           to: channelID,
-          message: giveCookie(id)
+          message: giveCookie(id, channelID)
         });
       break;
 
@@ -46,21 +58,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
         bot.sendMessage({
           to:channelID,
-          message: displayCookies(id)
+          message: displayCookies(id, channelID)
         });
       break;
-
-      case 'cookieinfo':
-        /*bot.sendMessage({
-          to: channelID,
-          message: getInfo(args[1])
-        })*/
-
     }
   }
 });
 
-function giveCookie(id)
+function giveCookie(id, channelID)
 {
   var users = JSON.parse( fs.readFileSync("./config/userCookies.json", "utf8") );
 
@@ -84,11 +89,11 @@ function giveCookie(id)
 
           fs.writeFileSync("./config/userCookies.json", JSON.stringify(users), "utf8")
 
-          return "Gave <@!" + id + "> a **rare** " + cookies.special[special].name + "!"
+          return ":cookie: Gave **" + bot.servers[bot.channels[channelID].guild_id].members[id].nick + "** a rare **" + cookies.special[special].name + "**!"
         }
       }
       fs.writeFileSync("./config/userCookies.json", JSON.stringify(users), "utf8")
-      return "Gave <@!" + id + "> 1 cookie!"
+      return ":cookie: Gave **" + bot.servers[bot.channels[channelID].guild_id].members[id].nick + "** 1 cookie!"
     }
   }
 
@@ -103,10 +108,10 @@ function giveCookie(id)
 
   fs.writeFileSync("./config/userCookies.json", JSON.stringify(users), "utf8")
 
-  return "Gave <@!" + id + "> 1 cookie and added them to the list!"
+  return ":cookie: Gave **" + bot.servers[bot.channels[channelID].guild_id].members[id].nick + "** 1 cookie and added them to the list!"
 }
 
-function displayCookies(id)
+function displayCookies(id, channelID)
 {
   var users = JSON.parse( fs.readFileSync("./config/userCookies.json", "utf8") );
 
@@ -119,7 +124,7 @@ function displayCookies(id)
   {
     if(users[i].id === id)
     {
-      var temp = "User Info:\nUsername: <@!" + users[i].id + ">\nCookies: " + users[i].cookies
+      var temp = "User Info:\nUsername: **" + bot.servers[bot.channels[channelID].guild_id].members[users[i].id].nick + "**\nCookies: " + users[i].cookies
 
       for(j in users[i].specialCookies)
       {
@@ -136,5 +141,5 @@ function displayCookies(id)
     }
   }
 
-  return "Sorry, " + id + " does not have any cookies :("
+  return "Sorry, " + bot.servers[bot.channels[channelID].guild_id].members[id].nick + " does not have any cookies :("
 }
